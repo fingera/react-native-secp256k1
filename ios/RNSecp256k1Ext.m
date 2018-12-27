fabf00f22f71c7250f98fe4ab35e7cd4f16d1561
liuyujun <liuyujun@fingera.cn>
2018-12-27 17:21:50 +0800

    update ios cryptor

--- ios/RNSecp256k1Ext.m
+++ ios/RNSecp256k1Ext.m
@@ -44,69 +44,74 @@ RCT_EXPORT_METHOD(generateKey:(RCTPromiseResolveBlock)resolve
     });
 }
 
-RCT_EXPORT_METHOD(encryptECDH:(NSString *)priv pub:(NSString *)pub data:(NSString *)data resolve:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject) {
+void ccc(bool encrypt, NSString *priv, NSString *pub, NSString *data, RCTPromiseResolveBlock resolve, RCTPromiseRejectBlock reject) {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         unsigned char ecdh[32];
         NSString *err = generateECDH(pub, priv, ecdh);
         if (err != nil) {
             reject(@"Error", err, nil);
         }
-        size_t numBytesEncrypted = 0;
         NSData *utf8 = [data dataUsingEncoding:NSUTF8StringEncoding];
-        size_t bufferSize = [utf8 length] * 3;
-        char *buffer = malloc(bufferSize);
-        CCCryptorStatus cryptStatus = CCCrypt(kCCEncrypt, kCCAlgorithmAES,
-                                              kCCOptionPKCS7Padding | kCCOptionECBMode,
-                                              ecdh, kCCKeySizeAES256,
-                                              NULL,
-                                              [utf8 bytes], [utf8 length],
-                                              buffer, bufferSize,
-                                              &numBytesEncrypted);
-        if (cryptStatus != kCCSuccess) {
-            free(buffer);
-            reject(@"Error", [NSString stringWithFormat:@"encrypt error: %d",(int)cryptStatus], nil);
+        const void *raw;
+        size_t rawLen;
+        if (encrypt) {
+            raw = [utf8 bytes];
+            rawLen = [utf8 length];
+        } else {
+            raw = malloc(from_base64_max_len([utf8 length]));
+            rawLen = from_base64([utf8 bytes], [utf8 length], (void *)raw);
+        }
+        
+        CCCryptorRef crypto = nil;
+        CCCryptorStatus status = CCCryptorCreate(encrypt ? kCCEncrypt : kCCDecrypt, kCCAlgorithmAES, kCCOptionPKCS7Padding | kCCOptionECBMode,
+                                                         ecdh, sizeof(ecdh), NULL, &crypto);
+        if (status != kCCSuccess) {
+            if (!encrypt) free((void *)raw);
+            if (crypto != nil) CCCryptorRelease(crypto);
+            reject(@"Error", [NSString stringWithFormat:@"cryptor create error: %d", (int)status], nil);
             return;
         }
-        resolveBase64(resolve, buffer, numBytesEncrypted);
-        free(buffer);
+        size_t outLen = CCCryptorGetOutputLength(crypto, rawLen, true);
+        size_t updateLen = 0;
+        size_t finalLen = 0;
+        char *out = malloc(outLen + 1);
+        status = CCCryptorUpdate(crypto, raw, rawLen, out, outLen, &updateLen);
+        if (status != kCCSuccess) {
+            if (!encrypt) free((void *)raw);
+            CCCryptorRelease(crypto);
+            free(out);
+            reject(@"Error", [NSString stringWithFormat:@"cryptor update error: %d", (int)status], nil);
+            return;
+        }
+        status = CCCryptorFinal(crypto, out + updateLen, outLen - updateLen, &finalLen);
+        if (status != kCCSuccess) {
+            if (!encrypt) free((void *)raw);
+            CCCryptorRelease(crypto);
+            free(out);
+            reject(@"Error", [NSString stringWithFormat:@"cryptor final error: %d", (int)status], nil);
+            return;
+        }
+        if (encrypt) {
+            resolveBase64(resolve, out, updateLen + finalLen);
+        } else {
+            out[updateLen + finalLen] = 0;
+            resolve([NSString stringWithUTF8String:out]);
+        }
+        if (!encrypt) free((void *)raw);
+        CCCryptorRelease(crypto);
+        free(out);
     });
-    
+}
+
+RCT_EXPORT_METHOD(encryptECDH:(NSString *)priv pub:(NSString *)pub data:(NSString *)data resolve:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+    ccc(true, priv, pub, data, resolve, reject);
 }
 
 
 RCT_EXPORT_METHOD(decryptECDH:(NSString *)priv pub:(NSString *)pub data:(NSString *)data resolve:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        unsigned char ecdh[32];
-        NSString *err = generateECDH(pub, priv, ecdh);
-        if (err != nil) {
-            reject(@"Error", err, nil);
-        }
-        NSData *utf8 = [data dataUsingEncoding:NSUTF8StringEncoding];
-        
-        char *raw = malloc(from_base64_max_len([utf8 length]));
-        size_t rawLen = from_base64([utf8 bytes], [utf8 length], raw);
-        
-        char *buffer = malloc(rawLen * 2);
-        size_t bufferSize = rawLen * 2;
-        size_t numBytesEncrypted = 0;
-        CCCryptorStatus cryptStatus = CCCrypt(kCCDecrypt, kCCAlgorithmAES,
-                                              kCCOptionPKCS7Padding | kCCOptionECBMode,
-                                              ecdh, kCCKeySizeAES256,
-                                              NULL,
-                                              raw, rawLen,
-                                              buffer, bufferSize,
-                                              &numBytesEncrypted);
-        if (cryptStatus != kCCSuccess) {
-            free(buffer);
-            reject(@"Error", [NSString stringWithFormat:@"decrypt error: %d",(int)cryptStatus], nil);
-            return;
-        }
-        buffer[numBytesEncrypted] = 0;
-        resolve([NSString stringWithUTF8String:buffer]);
-        free(buffer);
-    });
+    ccc(false, priv, pub, data, resolve, reject);
 }
 @end
 
