0da56086865a42798e7badab1c2e7eee1b0aef3e
liuyujun <liuyujun@fingera.cn>
2018-12-29 11:49:16 +0800

    improve android security

--- android/src/main/java/com/reactlibrary/RNSecp256k1Ext.java
+++ android/src/main/java/com/reactlibrary/RNSecp256k1Ext.java
@@ -9,20 +9,22 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
 import org.bitcoin.NativeSecp256k1;
-import org.bitcoin.NativeSecp256k1Util;
 
 import java.io.UnsupportedEncodingException;
 import java.security.SecureRandom;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class RNSecp256k1Ext extends ReactContextBaseJavaModule {
+    
+    SecureRandom GRandom = new SecureRandom();
 
-    private static byte[] Ase(byte[] byteData, byte[] byteKey, int opmode) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+    private static byte[] Ase(byte[] byteData, byte[] byteKey, int opmode, IvParameterSpec iv) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
         SecretKeySpec skeySpec = new SecretKeySpec(byteKey, "AES");
-        cipher.init(opmode, skeySpec);
+        cipher.init(opmode, skeySpec, iv);
         byte[] decrypted = cipher.doFinal(byteData);
         return decrypted;
     }
@@ -35,13 +37,37 @@ public class RNSecp256k1Ext extends ReactContextBaseJavaModule {
                     byte[] privraw = Base64.decode(priv, Base64.NO_PADDING);
                     byte[] pubraw = Base64.decode(pub, Base64.NO_PADDING);
                     byte[] sec = NativeSecp256k1.createECDHSecret(privraw, pubraw);
+                    byte[] eData = data;
+                    byte[] iv = new byte[]{
+                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    };
+                    IvParameterSpec ivSpec = new IvParameterSpec(iv);
+                    if (mode == Cipher.ENCRYPT_MODE) {
+                        int paddingLen = 16 - (data.length % 16);
+                        if (paddingLen < 2) {
+                            paddingLen += 16;
+                        }
+                        paddingLen--;
+                        byte[] random = new byte[paddingLen];
+                        GRandom.nextBytes(random);
+                        byte[] newData = new byte[random.length + data.length + 1];
+                        newData[0] = (byte)paddingLen;
+                        System.arraycopy(random, 0, newData, 1, random.length);
+                        System.arraycopy(data, 0, newData, 1 + random.length, data.length);
+                        eData = newData;
+                    }
 
-                    byte[] encryped = Ase(data, sec, mode);
+                    byte[] encryped = Ase(eData, sec, mode, ivSpec);
 
                     if (mode == Cipher.ENCRYPT_MODE) {
                         promise.resolve(Base64.encodeToString(encryped, Base64.NO_PADDING | Base64.NO_WRAP));
                     } else {
-                        promise.resolve(new String(encryped, "UTF-8"));
+                        // DoUnpadding
+                        int dataStart = (int)encryped[0] + 1;
+                        int realLen = encryped.length - dataStart;
+                        byte[] realData = new byte[realLen];
+                        System.arraycopy(encryped, dataStart, realData, 0, realLen);
+                        promise.resolve(new String(realData, "UTF-8"));
                     }
                 } catch (Exception ex) {
                     ex.printStackTrace();
@@ -67,9 +93,8 @@ public class RNSecp256k1Ext extends ReactContextBaseJavaModule {
             public void run() {
                 try {
                     byte[] privraw = new byte[32];
-                    SecureRandom random = new SecureRandom();
                     do {
-                        random.nextBytes(privraw);
+                        GRandom.nextBytes(privraw);
                     } while (!NativeSecp256k1.secKeyVerify(privraw));
                     promise.resolve(Base64.encodeToString(privraw, Base64.NO_PADDING | Base64.NO_WRAP));
                 } catch (Exception ex) {
